cac5d7a2f9fbedd27669567a4c7ddbf8c29ce2f3
sonyak <kch9141@naver.com>
2021-11-07 21:09:48 +0900

    finished sparta algorithm curriculum
    what a nice day

--- week_5/04_02_samsung1_baekjoon_style.py
+++ week_5/04_02_samsung1_baekjoon_style.py
@@ -1,53 +1,39 @@
 import copy
 
-k = 4  # 말의 개수
-
-chess_map = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-]
-start_horse_location_and_directions = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 2, 0],
-    [2, 2, 2]
-]
-# 이 경우는 게임이 끝나지 않아 -1 을 반환해야 합니다!
 # 동 서 북 남
 # →, ←, ↑, ↓
+# 맵의 번호가 1.1 로 시작한다는게 주위를 벽으로 둘러 쌓아도 괜찮다는 뜻처럼 여겨지네용
 dr = [0, 0, -1, 1]
 dy = [1, -1, 0, 0]
-
-## 좋은 트릭, 체스판의 주변을 모두 blue 로 만들면 벽을 만난다는 케이스를 뛰어넘기가 가능하다.
-### 첫번째 오류, 기본적인 작동은 잘 되었지만, 자기 턴에서 움직인게 아니라, 남의 턴에서 업혀서 움직였을 때 위치 정보 갱신이 이루어지지 않았기 때문에 오류 생김.--> 해결
-### 두번째 오류! 이중리스트는 다른 변수에 넣어 다른 객체로 쓰려고 해도 같은 객체이며 같은 메모리를 공유하게된다. 그래서 함수를 쓸수록 chess_map이 변했던것
-### 두번째 오류를 해결하기 위해서는, 이중리스트를 완전 복사해야되는데 이것은 그냥 변수할당이나, copy() 로 하면 안되고 deepcopy() 를 사용해아한다.
 def get_game_over_turn_count(horse_count, game_map, horse_location_and_directions):
     n_map = copy.deepcopy(game_map)
     new_map = surround_blue_wall(n_map) #-> 이중리스트 완전복사를 하여 같은 메모리를 공유해서 맵이 변하는 문제를 해결
     horse_info = horse_location_and_directions
     for i in horse_location_and_directions: # 맵을 둘러 쌓았기 때문에 생기는 위치 변화 체크
-        i[0] += 1
-        i[1] += 1
+        i[2] -= 1 # 방향을 0,1,2,3 으로 사용하기 위해
+
     current_board = [[[] for j in range(len(new_map))] for i in range(len(new_map))] # 체스판위에 어떤 말들이 위치해 있는지 저장하기 위함
     for i in range(len(horse_info)): # 초기 말들의 위치를 체스판위에 올려놓음
         r, y = horse_info[i][0], horse_info[i][1]
         current_board[r][y].append(i)
     count = 0
+    # print('처음 체스판', current_board)
     while count < 1000:
         count += 1
         for horse in range(horse_count):
             cur_r, cur_y = horse_info[horse][0], horse_info[horse][1] # 소환하려는 말의 위치.
+            # print('cur',cur_r, cur_y)
             new_r, new_y = horse_info[horse][0] + dr[horse_info[horse][2]], \
                            horse_info[horse][1] + dy[horse_info[horse][2]]  # 여기서 벽에 부딪힐때 체크해야됨 -> 맵을 파란색 보드로 둘러쌈으로써 문제해결
-
+            # print('new', new_r, new_y)
             if new_map[new_r][new_y] == 0: #가야할 곳이 흰색 땅일때
+                # print('흰색땅으로')
                 to_white_board(current_board, cur_r, cur_y, new_r, new_y, horse, horse_info)
             elif new_map[new_r][new_y] == 1: #가야할 곳이 빨간색 땅일때
+                # print('빨간땅으로')
                 to_red_board(current_board, cur_r, cur_y, new_r, new_y, horse, horse_info)
             elif new_map[new_r][new_y] == 2: # 가야할 곳이 파란색 땅일때
+                # print('파란땅으로', new_r, new_y)
                 change_direction(horse, horse_info) #반대 방향으로
                 new_r, new_y = horse_info[horse][0] + dr[horse_info[horse][2]], \
                          horse_info[horse][1] + dy[horse_info[horse][2]]  # 방향 반대인 상태에서 새로운 좌표
@@ -56,6 +42,7 @@ def get_game_over_turn_count(horse_count, game_map, horse_location_and_direction
                 elif new_map[new_r][new_y] == 1:
                     to_red_board(current_board, cur_r, cur_y, new_r, new_y, horse, horse_info)
 
+            # print(current_board)
             # 게임이 끝나는지 체크
             if len(current_board[new_r][new_y]) > 3:
                 return count
@@ -90,6 +77,7 @@ def to_red_board(board, r, y, nr, ny, horse, horse_info):
     board[r][y] = board[r][y][:i]  # 기존 보드 위에서 말이 움직이고 난 후를 표현한다.
 
 def change_direction(horse, horse_info):
+    # print('방향변화', horse, horse_info)
     horse_direction = horse_info[horse][2]
     if horse_direction == 0:
         horse_info[horse][2] = 1
@@ -100,21 +88,11 @@ def change_direction(horse, horse_info):
     elif horse_direction == 3:
         horse_info[horse][2] = 2   #말들의 방향을 반대로 한다.
 
+n, k = map(int, input().split()) # 체스판 크기 n*n, 말의 개수 k
 
-print(get_game_over_turn_count(k, chess_map, start_horse_location_and_directions))  # 2가 반환 되어야합니다
-# #
-start_horse_location_and_directions = [
-    [0, 1, 0],
-    [1, 1, 0],
-    [0, 2, 0],
-    [2, 2, 2]
-]
-print("정답 = 9 / 현재 풀이 값 = ", get_game_over_turn_count(k, chess_map, start_horse_location_and_directions))
-# #
-start_horse_location_and_directions = [
-    [0, 1, 0],
-    [0, 1, 1],
-    [0, 1, 0],
-    [2, 1, 2]
-]
-print("정답 = 3 / 현재 풀이 값 = ", get_game_over_turn_count(k, chess_map, start_horse_location_and_directions))
+chess_map = [list(map(int, input().split())) for r in range(n)]
+horse_info = [list(map(int, input().split())) for r in range(k)]
+
+# print(chess_map)
+# print(horse_info)
+print(get_game_over_turn_count(k, chess_map, horse_info))
